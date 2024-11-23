26606c5761acc9195fea413ed6105dc049eb251c
Evelyn <yourEmailHere>
2024-11-22 16:52:54 -0800

    update xcodeproject file

---
+++ AppetiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetiteApp.swift
+//  Appetite
+//
+//  Created by Evelyn Tran on 11/16/24.
+//
+
+import SwiftUI
+
+@main
+struct AppetiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Appetite
+//
+//  Created by Evelyn Tran on 11/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
